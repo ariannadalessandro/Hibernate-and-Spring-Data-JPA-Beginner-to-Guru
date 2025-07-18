8670034ccf47764ecf06b459a86ec5bb87ec024e
Arianna D'Alessandro <Ar.DAlessandro@almaviva.it>
2025-07-18 11:27:36 +0200

    inizializzazione

---
+++ src/main/java/com/hibernate_spring_data_jpa/demo_jpa/DemoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate_spring_data_jpa.demo_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibernate_spring_data_jpa/demo_jpa/DemoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate_spring_data_jpa.demo_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
